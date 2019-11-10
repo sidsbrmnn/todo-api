20df864619993d2556853d26068a56923f1b3b0b
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-11-10 19:34:05 +0530

    startup changed to services

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 
 const app = express();
 
-require('./startup/logging')();
-require('./startup/db')();
-require('./startup/routes')(app);
+require('./services/logging')();
+require('./services/db')();
+require('./services/routes')(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
